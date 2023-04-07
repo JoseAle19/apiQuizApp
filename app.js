66a89b52ebdde0ce11fda193d7f40d32add5fb61
jose alejandro <josealejandrocc22@gmail.com>
2023-04-07 02:58:47 -0500

    add cors3

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const port = process.env.PORT || 3000;
 //Midlewares
 app.use(express.json());
 app.use(index); // todas la rutas de index.js
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
   console.log(`http://localhost:${port}/`);
