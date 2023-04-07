ae955deb958ac1473861dc1ce76e996febaf70a0
jose alejandro <josealejandrocc22@gmail.com>
2023-04-07 03:01:45 -0500

    add cors4

--- app.js
+++ app.js
@@ -8,15 +8,9 @@ require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 //Midlewares
+app.use(cors());
 app.use(express.json());
 app.use(index); // todas la rutas de index.js
-app.use(cors());
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
   console.log(`http://localhost:${port}/`);
