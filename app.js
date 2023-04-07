4a307a85f2b9b0f916557af0e31ddc50b26b5c22
jose alejandro <josealejandrocc22@gmail.com>
2023-04-07 02:51:21 -0500

    add cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors(
   {
-    origin: "*",
+    origin: "http://127.0.0.1:5173/"
   }
 ));
 app.use(index); // todas la rutas de index.js
