262d96982daa0a654f0329e1624cd9845ef7cb51
jose alejandro <josealejandrocc22@gmail.com>
2023-04-07 02:29:03 -0500

    express

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ app.use(express.json());
 app.use(cors());
 app.use(index); // todas la rutas de index.js
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
 
-io.on("connection", (socket) => {
+// io.on("connection", (socket) => {
 
-  socket.on("respuesta", (data) => {
-    console.log(data)
-  });
+//   socket.on("respuesta", (data) => {
+//     console.log(data)
+//   });
    
-});
+// });
 
 
 
  
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Escuchando en el puerto ${port}`);
   console.log(`http://localhost:4000/`);
 });
